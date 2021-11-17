c7844924606e649744220b1bcc6f0346279688cc
Chidori <business.faissal@outlook.com>
2021-11-16 19:12:02 -0600

    refactoring start

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,23 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    // package up date as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    // send it as argument to createTaskEl
+    createTaskEl(taskDataObj);
+};
+
+
+var createTaskEl = function (taskDataObj) {
     // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
@@ -21,6 +33,7 @@ var createTaskHandler = function(event) {
     
     // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
-};
 
-formEl.addEventListener("submit", createTaskHandler);
+}
+
+formEl.addEventListener("submit", taskFormHandler);
